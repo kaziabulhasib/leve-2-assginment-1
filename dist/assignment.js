cd75f3cc01fed05d12d882957f3fb2155ff4eb17
kaziabulhasib <hasibkazi420@gmail.com>
2025-05-08 12:44:52 +0530

    final file setup

---
+++ dist/assignment.js
@@ -0,0 +1,72 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+function formatString(input, toUpper) {
+    if (toUpper === false) {
+        return input.toLowerCase();
+    }
+    else {
+        return input.toUpperCase();
+    }
+}
+function filterByRating(items) {
+    return items.filter((item) => item.rating >= 4);
+}
+function concatenateArrays(...arrays) {
+    return [].concat(...arrays);
+}
+class Vehicle {
+    constructor(make, year) {
+        this.make = make;
+        this.year = year;
+    }
+    getInfo() {
+        return `Make: ${this.make}, Year: ${this.year}`;
+    }
+}
+class Car extends Vehicle {
+    constructor(make, year, model) {
+        super(make, year);
+        this.model = model;
+    }
+    getModel() {
+        return `Model: ${this.model}`;
+    }
+}
+function processValue(value) {
+    return typeof value === "string" ? value.length : value * 2;
+}
+function getMostExpensiveProduct(products) {
+    if (products.length === 0)
+        return null;
+    return products.reduce((max, current) => current.price > max.price ? current : max);
+}
+var Day;
+(function (Day) {
+    Day[Day["Monday"] = 0] = "Monday";
+    Day[Day["Tuesday"] = 1] = "Tuesday";
+    Day[Day["Wednesday"] = 2] = "Wednesday";
+    Day[Day["Thursday"] = 3] = "Thursday";
+    Day[Day["Friday"] = 4] = "Friday";
+    Day[Day["Saturday"] = 5] = "Saturday";
+    Day[Day["Sunday"] = 6] = "Sunday";
+})(Day || (Day = {}));
+function getDayType(day) {
+    return day === Day.Saturday || day === Day.Sunday ? "Weekend" : "Weekday";
+}
+function squareAsync(n) {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (n < 0) {
+            throw new Error("Negative number not allowed");
+        }
+        yield new Promise((resolve) => setTimeout(resolve, 1000));
+        return n * n;
+    });
+}
